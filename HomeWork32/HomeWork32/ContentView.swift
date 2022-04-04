7590739b704c0b115cb3cfe5656ea2a5515e37d7
Дарья Дубровская <>
2022-04-04 20:53:47 +0300

    Task 32: File added

---
+++ HomeWork32/HomeWork32/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeWork32
+//
+//  Created by Дарья Дубровская on 4.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeWork32/HomeWork32/HomeWork32App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork32App.swift
+//  HomeWork32
+//
+//  Created by Дарья Дубровская on 4.04.22.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork32App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
